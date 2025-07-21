18b6ff6b36f8eb0a895d85f7b05096406ed447f7
112schauhan <surbhi.surbhisingh.singh13@gmail.com>
2025-07-21 17:34:14 +0530

    feat: Created basic express server with security middleware

---
+++ backend/src/server.ts
@@ -0,0 +1,29 @@
+import express from "express"
+import cors from "cors"
+import helmet from "helmet"
+import morgan from "morgan"
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(cors())
+app.use(helmet())
+app.use(morgan("combined"))
+app.use(express.json({ limit: '10mb'}));
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/health',(req,res)=>{
+    return res.status(200).json({
+        message:'Expense Tracker API is running'
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
+
+export default app;
+
+
