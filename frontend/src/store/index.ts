57efa4ceb9be247e7c49d2b3e8e126a4b7754ae1
112schauhan <surbhi.surbhisingh.singh13@gmail.com>
2025-07-24 12:56:18 +0530

    fix:Fixed the redux persist declaration issue

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -51,21 +51,3 @@ export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
-
-// import { configureStore } from "@reduxjs/toolkit"
-// import { useDispatch, useSelector, type TypedUseSelectorHook } from "react-redux"
-// import authReducer from "./authSlice"
-// import expenseReducer from "./expenseSlice"
-
-// export const store = configureStore({
-//   reducer: {
-//     auth: authReducer,
-//     expenses: expenseReducer,
-//   },
-// })
-
-// export type RootState = ReturnType<typeof store.getState>
-// export type AppDispatch = typeof store.dispatch
-
-// export const useAppDispatch = () => useDispatch<AppDispatch>()
-// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
